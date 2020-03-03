fa8ae48d7896e2aa392f0588bbfe393a33caed3f
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-03 18:10:29 -0500

    Initial Commit

---
+++ C0778923_MidTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0778923_MidTerm_MAD3115W2020
+//
+//  Created by Kritima Kukreja on 2020-03-03.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
