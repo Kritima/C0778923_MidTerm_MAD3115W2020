11523ede7ab313e6b0760ba2c6f77a62d505d890
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-03 20:54:00 -0500

    [KRITIMA]  Login button action function added

--- C0778923_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0778923_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -33,7 +33,41 @@ class LoginViewController: UIViewController {
             }
         }
     }
-}
+    @IBAction func btnLogin(_ sender: UIBarButtonItem) {
+        
+        let userOrEmail = usernameTextField.text
+               let password = passwordTextField.text
+               if (userOrEmail!.isEmpty || userOrEmail!.contains("")){
+                   showAlert(title: "Error !!", message: "Please Enter Valid Email or Username")
+                   return
+               }
+    
+            if (password!.isEmpty) || (password!.contains("")) {
+                showAlert(title: "Error !!", message: "Please Enter Valid Password")
+                return
+            }
+            
+            if userOrEmail == "admin" || password == "admin@123"{
+                let userdefault = UserDefaults.standard
+                if self.rememberMeSwitch.isOn {
+                    userdefault.set(self.usernameTextField.text,forKey:"userEmail")
+                    userdefault.set(self.passwordTextField.text, forKey: "pass")
+                }else{
+                    userdefault.removeObject(forKey: "userEmail")
+                    userdefault.removeObject(forKey: "pass")
+                }
+            }else{
+                showAlert(title: "Error !!", message: "Id or password is Invalid")
+                return
+            }
+            showCustomer()
+        }
+        
+    }
+
+
+
+
 
 
 
