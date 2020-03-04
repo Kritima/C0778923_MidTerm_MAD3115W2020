60e15dc21e42ba04743a9c102427b46505cfba0e
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-03 19:42:53 -0500

    [KRITIMA] Customer view controller scene added

---
+++ C0778923_MidTerm_MAD3115W2020/CustomerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerViewController.swift
+//  C0778923_MidTerm_MAD3115W2020
+//
+//  Created by Kritima Kukreja on 2020-03-03.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
