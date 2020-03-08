001e38e0b07cf8e404b9d47e4c69a4d281d99dce
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-08 18:17:52 -0400

    [KRTIMA] Final commit for part 1

--- C0778923_MidTerm_MAD3115W2020/CustomerDetailsViewController.swift
+++ C0778923_MidTerm_MAD3115W2020/CustomerDetailsViewController.swift
@@ -25,10 +25,11 @@ class CustomerDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Bill Details"
+        self.navigationItem.rightBarButtonItem?.title = "Add New Bill"
         
-        self.lblCustomerId.text = customerId
-        self.lblCustomerName.text = customerName
-        self.lblCustomerEmail.text = customerEmail
+        self.lblCustomerId.text = "Customer Id: \(customerId)"
+        self.lblCustomerName.text = "Customer Name: \(customerName)"
+        self.lblCustomerEmail.text = "Customer Email: \(customerEmail)"
 
         // Do any additional setup after loading the view.
     }
